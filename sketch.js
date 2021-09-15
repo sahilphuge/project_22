2840013e336a806a7170574ac3456500fab2343b
sahilphuge <89133048+sahilphuge@users.noreply.github.com>
2021-09-15 19:28:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,52 +2,26 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-
-var engine, world;
-var canvas;
-var player, playerBase;
-
+var engine, world,ground;
+var backgroundImg;
 
 
 function preload() {
-  backgroundImg = loadImage("./assets/background.png");
-  baseimage = loadImage("./assets/base.png");
-  playerimage = loadImage("./assets/player.png");
+ backgroundImg = loadImage("/assets/background.gif")
 }
-
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
 
+  canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-
-  angleMode(DEGREES);
-
-  var options = {
-    isStatic: true
-  };
-
-  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
-  World.add(world, playerBase);
-
-  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
-  World.add(world,player)
+  ground = new Ground(600,598,1200,1)
+ 
 }
 
 function draw() {
-  background(backgroundImg);
-
+  image(backgroundImg,0,0,1200,600)
+ 
   Engine.update(engine);
-  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
-  image(playerimage,player.position.x,player.position.y,50,180)
-
-  // Title
-  fill("#FFFF");
-  textAlign("center");
-  textSize(40);
-  text("EPIC ARCHERY", width / 2, 100);
+ ground.show() 
+   
 }
-
-
-
-
